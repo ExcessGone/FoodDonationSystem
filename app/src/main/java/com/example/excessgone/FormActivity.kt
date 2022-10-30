529bf5dccd759611577fd71b856e69ed24e08c7e
NourdotSiwar <nour.siwar@Outlook.com>
2022-10-30 08:57:04 -0700

    Added form - camera needs to be added

--- app/src/main/java/com/example/excessgone/FormActivity.kt
+++ app/src/main/java/com/example/excessgone/FormActivity.kt
@@ -2,20 +2,51 @@ package com.example.excessgone
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
+import com.example.excessgone.databinding.ActivityFormBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 
 class FormActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityFormBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_form)
+        initViews()
+    }
 
-
+    private fun initViews()
+    {
+        binding.submitBtn.setOnClickListener{ view ->
+            if(TextUtils.isEmpty(binding.textNameField.toString())){
+                binding.textNameInputLayout.error="Enter Name"
+                }
+            else if(TextUtils.isEmpty(binding.textEmailField.text.toString()))
+            {
+                binding.textNameInputLayout.isErrorEnabled = false
+                binding.textEmailInputLayout.error = "Enter Email"
+            }
+            else if(TextUtils.isEmpty(binding.textPhoneField.text.toString()))
+            {
+                binding.textEmailInputLayout.isErrorEnabled = false
+                binding.textPhoneInputLayout.error = "Enter Phone"
+            }
+            else if(TextUtils.isEmpty(binding.textAddressField.text.toString()))
+            {
+                binding.textPhoneInputLayout.isErrorEnabled = false
+                binding.textAddressInputLayout.error = "Enter Address"
+            }
+            else
+            {
+                binding.textAddressInputLayout.isErrorEnabled = false
+            }
+        }
     }
 }
