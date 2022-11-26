9a6aafa2405d52a97c3dae3e25ba8ba02f810988
Nour Siwar <100004210+NourdotSiwar@users.noreply.github.com>
2022-11-26 13:37:58 -0800

    Update FormAdapter.kt

--- app/src/main/java/com/example/excessgone/FormAdapter.kt
+++ app/src/main/java/com/example/excessgone/FormAdapter.kt
@@ -55,7 +55,6 @@ import com.bumptech.glide.Glide
 import com.example.excessgone.R
 import kotlinx.android.synthetic.main.form_item.view.*
 
-
 class MyAdapter(
     private val urls: List<String>
 ) : RecyclerView.Adapter<MyAdapter.ImageViewHolder>() {
@@ -80,89 +79,4 @@ class MyAdapter(
         Glide.with(holder.itemView).load(url).into(holder.itemView.foodImage)
     }
 }
-
-
-
-
-
-
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-class MyAdapter(private val context: Context, private val formList: List<Forms>) : RecyclerView.Adapter<MyAdapter.MyViewHolder>(){
-
-   // private val formList = ArrayList<Forms>()
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-      val itemView = LayoutInflater.from(parent.context).inflate(
-          R.layout.form_item,
-          parent, false
-      )
-        return MyViewHolder(itemView)
-    }
-
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val currentItem = formList[position]
-        holder.bind(currentItem)
-
-        //shelterNameTextView.text=currentItem.shelterName
-
-       // holder.phoneNumber.text=currentItem.phoneNumber
-       // holder.address.text=currentItem.address
-       // holder.foodType.text=currentItem.foodType
-    }
-
- //   override fun getItemCount() = formList.size
-
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener{
-
-        private val foodImageView = itemView.findViewById<ImageView>(R.id.foodImage)
-        private val shelterNameTextView : TextView = itemView.findViewById(R.id.nameField)
-
-        init{
-            itemView.setOnClickListener(this)
-        }
-
-        fun bind( currentItem : Forms){
-            shelterNameTextView.text = currentItem.toString()
-
-            Glide.with(context)
-                .loard(currentItem.)
-        }
-    }
-
-    fun updateFormList(formList : List<Forms>){
-        this.formList.clear()
-        this.formList.addAll(formList)
-        notifyDataSetChanged()
-    }
-
-    class MyViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
-
-        val shelterNameTextView : TextView = itemView.findViewById(R.id.nameField)
-       // val foodImageTextView : TextView = itemView.findViewById(R.id.imageFood)
-
-
-    }
-}
-
- */
